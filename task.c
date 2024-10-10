aff8e33129822f7df5e467bd32feb6d8103af87a
Name-CK <3126127106@qq.com>
2024-10-10 19:48:01 +0800

    第一次提交

--- task.c
+++ task.c
@@ -1,4 +1,4 @@
-
+#include "REGX52.h"
 
 void delay_ms(unsigned int x)  // 延时函数
 {
@@ -21,10 +21,39 @@ int main(void)
     """
     在下方编写你的代码
     """
-    
+    //方式1
+    P0 = 0x7F;//0111 1111
+    while(1)
+    {
+        delay_ms(500);
+        P0 = P0>>1;
+        P0 += 0x80;//1000 0000
+        if(P0==0xFF)//1111 1111
+        {
+             P0 = 0x7F;//0111 1111
+        }
 
- 
-
+    }
+    //方式2
+    while(0)//不执行
+    {
+        P0 = 0x7F;//0111 1111
+        delay_ms(500);
+        P0 = 0xBF;//1011 1111
+        delay_ms(500);
+        P0 = 0xDF;//1101 1111
+        delay_ms(500);
+        P0 = 0xEF;//1110 1111
+        delay_ms(500);
+        P0 = 0xF7;
+        delay_ms(500);
+        P0 = 0xFB;
+        delay_ms(500);
+        P0 = 0xFD;
+        delay_ms(500);
+        P0 = 0xFE;
+        delay_ms(500);
+    }
     return 0;
 }
 
