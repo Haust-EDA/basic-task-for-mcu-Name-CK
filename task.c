ca585187a6d4c70c42027efbb465af813824ceca
Name-CK <3126127106@qq.com>
2024-10-11 12:37:16 +0800

    使用预编译优化

--- task.c
+++ task.c
@@ -1,57 +1,60 @@
 #include "REGX52.h"
 
-void delay_ms(unsigned int x)  // 延时函数
+void delay_ms(unsigned int x) // 延时函数
 {
-    unsigned int i,j;
-    if(x==1000)
+    unsigned int i, j;
+    if (x == 1000)
     {
-        for(i=0;i<19601;i++)//延时1s
+        for (i = 0; i < 19601; i++) // 延时1s
         {
-            for(j=5;j>0;j--);
+            for (j = 5; j > 0; j--);
         }
     }
-    else while(x--)for(j=115;j>0;j--);
+    else while (x--)for (j = 115; j > 0; j--);
 }
 
 // tips: 原理图当中led为低电平点亮，比如点亮LED2,代码为： P0 = 0xFE (1111 1110)
 
-
 int main(void)
 {
     """
-    在下方编写你的代码
+        在下方编写你的代码
     """
-    //配置74HC138，使IO6为低电平
-    P1^4=0;
-    P1^3=1;
-    P1^0=0;
-    P1^1=1;
-    P1^2=1;
+    // 配置74HC138，使IO6为低电平
+    P1^4 = 0;
+    P1^3 = 1;
+    P1^0 = 0;
+    P1^1 = 1;
+    P1^2 = 1;
 
-    //流水灯
-    //方式1
-    P0 = 0x7F;//0111 1111
-    while(1)
+// 流水灯
+#define Use_Mode1
+
+/*方式1*/
+#ifdef Use_Mode1
+    P0 = 0x7F; // 0111 1111
+    while (1)
     {
         delay_ms(500);
-        P0 = P0>>1;
-        P0 += 0x80;//1000 0000
-        if(P0==0xFF)//1111 1111
+        P0 = P0 >> 1;
+        P0 += 0x80;     // 1000 0000
+        if (P0 == 0xFF) // 1111 1111
         {
-             P0 = 0x7F;//0111 1111
+            P0 = 0x7F; // 0111 1111
         }
-
     }
-    //方式2
-    while(0)//不执行
+
+/*方式2*/
+#else
+    while (0) // 不执行
     {
-        P0 = 0x7F;//0111 1111
+        P0 = 0x7F; // 0111 1111
         delay_ms(500);
-        P0 = 0xBF;//1011 1111
+        P0 = 0xBF; // 1011 1111
         delay_ms(500);
-        P0 = 0xDF;//1101 1111
+        P0 = 0xDF; // 1101 1111
         delay_ms(500);
-        P0 = 0xEF;//1110 1111
+        P0 = 0xEF; // 1110 1111
         delay_ms(500);
         P0 = 0xF7;
         delay_ms(500);
@@ -62,6 +65,6 @@ int main(void)
         P0 = 0xFE;
         delay_ms(500);
     }
+#endif
     return 0;
 }
-
